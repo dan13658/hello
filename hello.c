27c7548242d26c4ecf7d01a2facb8f036564946d
Gavin <gavin.grabill@huskers.unl.edu>
2019-08-29 08:34:24 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
 int main(int argc, char **argv) {
    printf("Daniel Sahn\n");
 +  printf("Economics & Merchandising\n", );
+   printf("Gavin Grabill\n");
+   printf("Actuarial Science\n");
   return0;
 }
